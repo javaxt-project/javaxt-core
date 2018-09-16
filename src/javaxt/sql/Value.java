0bf0dac1fd8db3b2e10def4a5e6350d04d0022aa
pborissow <email@pborissow.dev>
2018-09-16 10:53:39 +0000

    - Added support for CLOB values in the toString() method
    git-svn-id: svn://192.168.0.80/JavaXT/javaxt-core@1098 2c7b0aa6-e0b2-3c4e-bb4a-8b65b6c465ff

--- src/javaxt/sql/Value.java
+++ src/javaxt/sql/Value.java
@@ -21,6 +21,13 @@ public class Value extends javaxt.utils.Value {
         super(value);
     }
 
+    
+  //**************************************************************************
+  //** toTimeStamp
+  //**************************************************************************
+  /** Returns the value as a java.sql.Timestamp. Returns a null if there was a 
+   *  problem converting the value to a Timestamp or if the value is null.
+   */
     public java.sql.Timestamp toTimeStamp(){
         Object obj = super.toObject();
         if (obj!=null){
@@ -35,6 +42,14 @@ public class Value extends javaxt.utils.Value {
         return null;
     }
 
+    
+  //**************************************************************************
+  //** toArray
+  //**************************************************************************
+  /** If the value is a java.sql.Array, returns the output from the 
+   *  java.sql.Array.getArray() method. Returns a null if there was a problem 
+   *  converting the value to a java.sql.Array or if the value is null.
+   */
     public Object toArray(){
         Object obj = super.toObject();
         if (obj!=null){
@@ -45,6 +60,48 @@ public class Value extends javaxt.utils.Value {
             }
         }
 
+        return null;
+    }
+    
+    
+  //**************************************************************************
+  //** toString
+  //**************************************************************************
+  /** Returns the value as a String. Returns a null if there was a problem 
+   *  converting the value to a String or if the value is null.
+   */
+    public String toString(){
+        Object obj = super.toObject();
+        if (obj!=null){
+            if (obj instanceof java.sql.Clob){
+                String str = null;
+                java.sql.Clob clob = (java.sql.Clob) obj;
+                try{
+                    long len = clob.length();
+                    if (len < Integer.MIN_VALUE || len > Integer.MAX_VALUE){
+                        //CLOB size too big for String!
+                    }
+                    else{
+                        str = clob.getSubString(1, (int) clob.length());
+                    }
+                }
+                catch(Exception e){
+                }
+                finally{
+                    if (clob!=null){
+                        try{
+                            clob.free();
+                        }
+                        catch(Exception e){}
+                    }
+                }
+                return str;
+            }
+            else{
+                return obj.toString();
+            }
+        }
+        
         return null;
     }
 }
