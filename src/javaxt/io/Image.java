0bbbd5097f3fda05cdd60b2cfcc3b4394ed79951
pborissow <email@pborissow.dev>
2020-09-19 21:14:37 +0000

    - Added blur() method to the Image class
    git-svn-id: svn://192.168.0.80/JavaXT/javaxt-core@1311 2c7b0aa6-e0b2-3c4e-bb4a-8b65b6c465ff

--- src/javaxt/io/Image.java
+++ src/javaxt/io/Image.java
@@ -876,7 +876,7 @@ public class Image {
         int width = this.getWidth();
         int height = this.getHeight();
 
-      //define kernal
+      //define kernel
         Kernel kernel = new Kernel(3, 3,
             new float[] {
              0.0f, -0.2f,  0.0f,
@@ -893,14 +893,129 @@ public class Image {
         Graphics2D g2d = bufferedImage.createGraphics();
         g2d.drawImage(overlay,2,2,null);
         g2d.dispose();
+    }
 
+
+  //**************************************************************************
+  //** blur
+  //**************************************************************************
+  /** Used to blur the image using a Gaussian kernel.
+   */
+    public void blur(float radius){
+
+
+      //Create kernel
+        int r = (int)Math.ceil(radius);
+        int rows = r*2+1;
+        float[] matrix = new float[rows];
+        float sigma = radius/3;
+        float sigma22 = 2*sigma*sigma;
+        float sigmaPi2 = (float)(2*Math.PI*sigma);
+        float sqrtSigmaPi2 = (float)Math.sqrt(sigmaPi2);
+        float radius2 = radius*radius;
+        float total = 0;
+        int index = 0;
+        for (int row = -r; row <= r; row++) {
+            float distance = row*row;
+            if (distance > radius2)
+                matrix[index] = 0;
+            else
+                matrix[index] = (float)Math.exp(-(distance)/sigma22) / sqrtSigmaPi2;
+            total += matrix[index];
+            index++;
+        }
+        for (int i = 0; i < rows; i++)
+            matrix[i] /= total;
+
+        Kernel kernel = new Kernel(rows, 1, matrix);
+
+
+      //
+        int width = this.getWidth();
+        int height = this.getHeight();
+        int[] inPixels = new int[width*height];
+        int[] outPixels = new int[width*height];
+        bufferedImage.getRGB( 0, 0, width, height, inPixels, 0, width );
+        convolveAndTranspose(kernel, inPixels, outPixels, width, height, true, CLAMP_EDGES);
+        convolveAndTranspose(kernel, outPixels, inPixels, height, width, true, CLAMP_EDGES);
+        bufferedImage.setRGB( 0, 0, width, height, inPixels, 0, width );
+    }
+
+    private static int CLAMP_EDGES = 1;
+    private static int WRAP_EDGES = 2;
+
+
+  //**************************************************************************
+  //** convolveAndTranspose
+  //**************************************************************************
+  /** Applies 1D Gaussian kernel used to blur an image. This filter should be
+   *  applied twice, once horizontally and once vertically.
+   *  @author Jerry Huxtable
+   */
+    private static void convolveAndTranspose(Kernel kernel, int[] inPixels, int[] outPixels,
+        int width, int height, boolean alpha, int edgeAction) {
+
+
+        float[] matrix = kernel.getKernelData( null );
+        int cols = kernel.getWidth();
+        int cols2 = cols/2;
+
+        for (int y = 0; y < height; y++) {
+            int index = y;
+            int ioffset = y*width;
+            for (int x = 0; x < width; x++) {
+                float r = 0, g = 0, b = 0, a = 0;
+                int moffset = cols2;
+                for (int col = -cols2; col <= cols2; col++) {
+                    float f = matrix[moffset+col];
+
+                    if (f != 0) {
+                        int ix = x+col;
+                        if ( ix < 0 ) {
+                            if ( edgeAction == CLAMP_EDGES )
+                                    ix = 0;
+                            else if ( edgeAction == WRAP_EDGES )
+                                    ix = (x+width) % width;
+                        } else if ( ix >= width) {
+                            if ( edgeAction == CLAMP_EDGES )
+                                    ix = width-1;
+                            else if ( edgeAction == WRAP_EDGES )
+                                    ix = (x+width) % width;
+                        }
+                        int rgb = inPixels[ioffset+ix];
+                        a += f * ((rgb >> 24) & 0xff);
+                        r += f * ((rgb >> 16) & 0xff);
+                        g += f * ((rgb >> 8) & 0xff);
+                        b += f * (rgb & 0xff);
+                    }
+                }
+                int ia = alpha ? clamp((int)(a+0.5)) : 0xff;
+                int ir = clamp((int)(r+0.5));
+                int ig = clamp((int)(g+0.5));
+                int ib = clamp((int)(b+0.5));
+                outPixels[index] = (ia << 24) | (ir << 16) | (ig << 8) | ib;
+                index += height;
+            }
+        }
+    }
+
+
+  //**************************************************************************
+  //** clamp
+  //**************************************************************************
+  /** Clamp a value to the range 0..255
+   */
+    private static int clamp(int c) {
+        if (c < 0) return 0;
+        if (c > 255) return 255;
+        return c;
     }
 
 
   //**************************************************************************
   //** Desaturate
   //**************************************************************************
-  /**  Used to completely desaturate an image (creates a gray-scale image).
+  /** Used to completely desaturate an image (creates a gray-scale image).
    */
     public void desaturate(){
          bufferedImage = desaturate(bufferedImage);
@@ -910,9 +1025,9 @@ public class Image {
   //**************************************************************************
   //** Desaturate
   //**************************************************************************
-  /**  Used to desaturate an image by a specified percentage (expressed as
-   *   a double or float). The larger the percentage, the greater the
-   *   desaturation and the "grayer" the image. Valid ranges are from 0-1.
+  /** Used to desaturate an image by a specified percentage (expressed as
+   *  a double or float). The larger the percentage, the greater the
+   *  desaturation and the "grayer" the image. Valid ranges are from 0-1.
    */
     public void desaturate(double percent){
         float alpha = (float) (percent);
