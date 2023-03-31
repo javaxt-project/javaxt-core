940de43eb6bb628440188f1a84b7620831a9ed87
pborissow <email@pborissow.dev>
2023-03-31 18:52:04 +0000

    Updated Generator class to be AutoCloseable
    git-svn-id: svn://192.168.0.80/JavaXT/javaxt-core@1495 2c7b0aa6-e0b2-3c4e-bb4a-8b65b6c465ff

--- src/javaxt/utils/Generator.java
+++ src/javaxt/utils/Generator.java
@@ -43,110 +43,112 @@ import java.util.NoSuchElementException;
  *
  ******************************************************************************/
 
-public abstract class Generator<T> implements Iterable<T> {
+public abstract class Generator<T> implements Iterable<T>, AutoCloseable {
 
-	private class Condition {
-		private boolean isSet;
-		public synchronized void set() {
-			isSet = true;
-			notify();
-		}
-		public synchronized void await() throws InterruptedException {
-			try {
-				if (isSet)
-					return;
-				wait();
-			} finally {
-				isSet = false;
-			}
-		}
-	}
+    private class Condition {
+        private boolean isSet;
+        public synchronized void set() {
+            isSet = true;
+            notify();
+        }
+        public synchronized void await() throws InterruptedException {
+            try {
+                if (isSet)
+                        return;
+                wait();
+            } finally {
+                isSet = false;
+            }
+        }
+    }
 
-	static ThreadGroup THREAD_GROUP;
+    static ThreadGroup THREAD_GROUP;
 
-	Thread producer;
-	private boolean hasFinished;
-	private final Condition itemAvailableOrHasFinished = new Condition();
-	private final Condition itemRequested = new Condition();
-	private T nextItem;
-	private boolean nextItemAvailable;
-	private RuntimeException exceptionRaisedByProducer;
+    Thread producer;
+    private boolean hasFinished;
+    private final Condition itemAvailableOrHasFinished = new Condition();
+    private final Condition itemRequested = new Condition();
+    private T nextItem;
+    private boolean nextItemAvailable;
+    private RuntimeException exceptionRaisedByProducer;
 
-	@Override
-	public Iterator<T> iterator() {
-		return new Iterator<T>() {
-			@Override
-			public boolean hasNext() {
-				return waitForNext();
-			}
-			@Override
-			public T next() {
-				if (!waitForNext())
-					throw new NoSuchElementException();
-				nextItemAvailable = false;
-				return nextItem;
-			}
-			@Override
-			public void remove() {
-				throw new UnsupportedOperationException();
-			}
-			private boolean waitForNext() {
-				if (nextItemAvailable)
-					return true;
-				if (hasFinished)
-					return false;
-				if (producer == null)
-					startProducer();
-				itemRequested.set();
-				try {
-					itemAvailableOrHasFinished.await();
-				} catch (InterruptedException e) {
-					hasFinished = true;
-				}
-				if (exceptionRaisedByProducer != null)
-					throw exceptionRaisedByProducer;
-				return !hasFinished;
-			}
-		};
-	}
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                    return waitForNext();
+            }
+            @Override
+            public T next() {
+                if (!waitForNext())
+                        throw new NoSuchElementException();
+                nextItemAvailable = false;
+                return nextItem;
+            }
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+            private boolean waitForNext() {
+                if (nextItemAvailable)
+                        return true;
+                if (hasFinished)
+                        return false;
+                if (producer == null)
+                        startProducer();
+                itemRequested.set();
+                try {
+                        itemAvailableOrHasFinished.await();
+                } catch (InterruptedException e) {
+                        hasFinished = true;
+                }
+                if (exceptionRaisedByProducer != null)
+                        throw exceptionRaisedByProducer;
+                return !hasFinished;
+            }
+        };
+    }
 
-	protected abstract void run() throws InterruptedException;
+    public void close(){}
 
-	protected void yield(T element) throws InterruptedException {
-		nextItem = element;
-		nextItemAvailable = true;
-		itemAvailableOrHasFinished.set();
-		itemRequested.await();
-	}
+    protected abstract void run() throws InterruptedException;
 
-	private void startProducer() {
-		assert producer == null;
-		if (THREAD_GROUP == null)
-			THREAD_GROUP = new ThreadGroup("generatorfunctions");
-		producer = new Thread(THREAD_GROUP, new Runnable() {
-			@Override
-			public void run() {
-				try {
-					itemRequested.await();
-					Generator.this.run();
-				} catch (InterruptedException e) {
-					// No need to do anything here; Remaining steps in run()
-					// will cleanly shut down the thread.
-				} catch (RuntimeException e) {
-					exceptionRaisedByProducer = e;
-				}
-				hasFinished = true;
-				itemAvailableOrHasFinished.set();
-			}
-		});
-		producer.setDaemon(true);
-		producer.start();
-	}
+    protected void yield(T element) throws InterruptedException {
+        nextItem = element;
+        nextItemAvailable = true;
+        itemAvailableOrHasFinished.set();
+        itemRequested.await();
+    }
 
-	@Override
-	protected void finalize() throws Throwable {
-		producer.interrupt();
-		producer.join();
-		super.finalize();
-	}
+    private void startProducer() {
+        assert producer == null;
+        if (THREAD_GROUP == null)
+                THREAD_GROUP = new ThreadGroup("generatorfunctions");
+        producer = new Thread(THREAD_GROUP, new Runnable() {
+                @Override
+                public void run() {
+                        try {
+                                itemRequested.await();
+                                Generator.this.run();
+                        } catch (InterruptedException e) {
+                                // No need to do anything here; Remaining steps in run()
+                                // will cleanly shut down the thread.
+                        } catch (RuntimeException e) {
+                                exceptionRaisedByProducer = e;
+                        }
+                        hasFinished = true;
+                        itemAvailableOrHasFinished.set();
+                }
+        });
+        producer.setDaemon(true);
+        producer.start();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        producer.interrupt();
+        producer.join();
+        super.finalize();
+    }
 }
